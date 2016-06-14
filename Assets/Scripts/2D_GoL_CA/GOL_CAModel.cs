abc5ca69ebebc13fefd5ed4b2a6f21590e0fb007
Jordan Kisiel <jordan.kisiel@gmail.com>
2016-06-13 21:28:59 -0400

    Added script files for standard Game of Life CA with hard-code rule set

---
+++ Assets/Scripts/2D_GoL_CA/GOL_CAModel.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GOL_CAModel : MonoBehaviour {
+
+	public int cellsDimensionX;
+	public int cellsDimensionY;
+	public int seed;
+	[Range(0.01f, 0.99f)] public float percentLiving;
+	public float timeInterval;
+
+	private bool[,] cells;
+
+	// Use this for initialization
+	void Start () {
+		cells = new bool[cellsDimensionX, cellsDimensionY];
+
+		GenerateRandomState(seed);
+
+		InvokeRepeating("GenerateNextTimeStep", 0, timeInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	private void GenerateRandomState(int seed){
+		Random.seed = seed;
+
+		for(int i = 0; i < cellsDimensionX; i++){
+			for(int j = 0; j < cellsDimensionY; j++){
+				cells[i, j] = Random.value <= percentLiving;
+			}
+		}
+	}
+
+	private void GenerateNextTimeStep(){
+		bool[,] cellsCopy = (bool[,]) cells.Clone();
+
+		for(int i = 0; i < cellsDimensionX; i++){
+			for(int j = 0; j < cellsDimensionY; j++){
+				bool[] neighbors = GetCellNeighbors(i, j, cellsCopy);
+
+				int livingCount = 0;
+				for(int x = 0; x < neighbors.Length; x++){
+					if(neighbors[x] == true){
+						livingCount += 1;
+					}
+				}
+
+				bool selectedCell = cellsCopy[i, j];
+
+				//underpopulation rule
+				if(livingCount < 2 && selectedCell){
+					cells[i, j] = false;
+				}
+				//survival
+				else if((livingCount == 2 || livingCount == 3) && selectedCell){
+					cells[i, j] = true;
+				}
+				//overpopulation rule
+				else if(livingCount > 3 && selectedCell){
+					cells[i, j] = false;
+				}
+				//birth rule
+				else if(livingCount == 3 && !selectedCell){
+					cells[i, j] = true;
+				}
+			}
+		}
+	}
+
+	//uses Moore neighborhood
+	private bool[] GetCellNeighbors(int xCoord, int yCoord, bool[,] cells){
+		bool[] cellNeighbors = new bool[8];
+
+		//west cell
+		if(xCoord != 0){
+			cellNeighbors[0] = cells[xCoord - 1, yCoord];
+		}else{
+			cellNeighbors[0] = cells[cellsDimensionX - 1, yCoord];
+		}
+
+		//east cell
+		if(xCoord != cellsDimensionX - 1){
+			cellNeighbors[1] = cells[xCoord + 1, yCoord];
+		}else{
+			cellNeighbors[1] = cells[0, yCoord];
+		}
+
+		//north cell
+		if(yCoord != 0){
+			cellNeighbors[2] = cells[xCoord, yCoord - 1];
+		}else{
+			cellNeighbors[2] = cells[xCoord, cellsDimensionY - 1];
+		}
+
+		//south cell
+		if(yCoord != cellsDimensionY - 1){
+			cellNeighbors[3] = cells[xCoord, yCoord + 1];
+		}else{
+			cellNeighbors[3] = cells[xCoord, 0];
+		}
+
+		//northwest cell
+		if(xCoord != 0 && yCoord != 0){
+			cellNeighbors[4] = cells[xCoord - 1, yCoord - 1];
+		}else if(xCoord != 0){
+			cellNeighbors[4] = cells[xCoord - 1, cellsDimensionY - 1];
+		}else if(yCoord != 0){
+			cellNeighbors[4] = cells[cellsDimensionX - 1, yCoord - 1];
+		}else{
+			cellNeighbors[4] = cells[cellsDimensionX - 1, cellsDimensionY - 1];
+		}
+
+		//northeast cell
+		if(xCoord != cellsDimensionX - 1 && yCoord != 0){
+			cellNeighbors[5] = cells[xCoord + 1, yCoord - 1];
+		}else if(xCoord != cellsDimensionX - 1){
+			cellNeighbors[5] = cells[xCoord + 1, cellsDimensionY - 1];
+		}else if(yCoord != 0){
+			cellNeighbors[5] = cells[0, yCoord - 1];
+		}else{
+			cellNeighbors[5] = cells[0, cellsDimensionY - 1];
+		}
+
+		//southeast cell
+		if(xCoord != cellsDimensionX - 1 && yCoord != cellsDimensionY - 1){
+			cellNeighbors[6] = cells[xCoord + 1, yCoord + 1];
+		}else if(xCoord != cellsDimensionX - 1){
+			cellNeighbors[6] = cells[xCoord + 1, 0];
+		}else if(yCoord != cellsDimensionY - 1){
+			cellNeighbors[6] = cells[0, yCoord + 1];
+		}else{
+			cellNeighbors[6] = cells[0, 0];
+		}
+
+		//southwest cell
+		if(xCoord != 0 && yCoord != cellsDimensionY - 1){
+			cellNeighbors[7] = cells[xCoord - 1, yCoord + 1];
+		}else if(xCoord != 0){
+			cellNeighbors[7] = cells[xCoord - 1, 0];
+		}else if(yCoord != cellsDimensionY - 1){
+			cellNeighbors[7] = cells[cellsDimensionX - 1, yCoord + 1];
+		}else{
+			cellNeighbors[7] = cells[cellsDimensionX - 1, 0];
+		}
+
+		return cellNeighbors;
+	}
+
+	public bool GetCell(int xCoord, int yCoord){
+		return cells[xCoord, yCoord];
+	}
+}
---
+++ Assets/Scripts/2D_GoL_CA/GOL_CAView.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(GOL_CAModel))]
+public class GOL_CAView : MonoBehaviour {
+
+	public float cellBuffer;
+	public float cellSize;
+	public GameObject cellRepresentation;
+
+	private GOL_CAModel caModel;
+
+	// Use this for initialization
+	void Start () {
+		caModel = gameObject.GetComponent<GOL_CAModel>();
+
+		for(int i = 0; i < caModel.cellsDimensionX; i++){
+			GameObject cellRow = new GameObject();
+
+			cellRow.transform.parent = gameObject.transform;
+			cellRow.name = "Cell Row " + i;
+
+			for(int j = 0; j < caModel.cellsDimensionY; j++){
+				GameObject cell = Instantiate(cellRepresentation, Vector3.zero, Quaternion.identity) as GameObject;
+
+				cell.transform.parent = cellRow.transform;
+				cell.name = "Cell " + j;
+
+				float xPos = j + (float)(cellSize/2) + (j * cellBuffer);
+				cell.transform.position = new Vector2(xPos, 0);
+			}
+
+			float rowPosY = i + (float)(cellSize + (i * cellBuffer));
+			cellRow.transform.position = new Vector2(0, rowPosY);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < caModel.cellsDimensionX; i++) {
+			for (int j = 0; j < caModel.cellsDimensionY; j++) {
+				gameObject.transform.GetChild(i).GetChild(j).gameObject.SetActive(caModel.GetCell(i, j));
+			}
+		}
+	}
+}
