8537a51a30510f47ae7fd8552cabaa04d5fc19b4
Jordan Kisiel <jordan.kisiel@gmail.com>
2016-06-16 20:30:35 -0400

    Added scripts for Continuous Value cellular automata

---
+++ Assets/Scripts/ConValue_CA/ConValue_CAModel.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConValue_CAModel : MonoBehaviour {
+
+	public int cellsDimensionX;
+	public int cellsDimensionY;
+	public int seed;
+	[Range(0.0f, 1.0f)]public float addingConstant;
+	public float timeInterval;
+
+	private float[,] cells;
+
+	// Use this for initialization
+	void Start () {
+		cells = new float[cellsDimensionX, cellsDimensionY];
+
+		Random.seed = seed;
+		GenerateRandomState();
+
+		InvokeRepeating("GenerateNextTimeStep", 0, timeInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void GenerateRandomState(){
+		for(int i = 0; i < cellsDimensionX; i++){
+			for(int j = 0; j < cellsDimensionY; j++){
+				cells[i, j] = Random.value;
+			}
+		}
+	}
+
+	private void GenerateNextTimeStep(){
+		float[,] cellsCopy = (float[,]) cells.Clone();
+
+		for(int i = 0; i < cellsDimensionX; i++){
+			for(int j = 0; j < cellsDimensionY; j++){
+				float[] neighbors = GetCellNeighbors(i, j, cellsCopy);
+
+				float selectedCell = cellsCopy[i, j];
+
+				cells[i, j] = ApplyRules(selectedCell, neighbors);
+			}
+		}
+	}
+
+	private float ApplyRules(float selectedCell, float[] neighbors){
+		float average = selectedCell;
+
+		for(int i = 0; i < neighbors.Length; i++){
+			average += neighbors[i];
+		}
+
+		average = average / 9.0f;
+
+		double sum = average + addingConstant;
+
+		return (float)(sum - System.Math.Truncate(sum));
+	}
+
+	//uses Moore neighborhood
+	private float[] GetCellNeighbors(int xCoord, int yCoord, float[,] cells){
+		float[] cellNeighbors = new float[8];
+
+		//west cell
+		if(xCoord != 0){
+			cellNeighbors[0] = cells[xCoord - 1, yCoord];
+		}else{
+			cellNeighbors[0] = cells[cellsDimensionX - 1, yCoord];
+		}
+
+		//east cell
+		if(xCoord != cellsDimensionX - 1){
+			cellNeighbors[1] = cells[xCoord + 1, yCoord];
+		}else{
+			cellNeighbors[1] = cells[0, yCoord];
+		}
+
+		//north cell
+		if(yCoord != 0){
+			cellNeighbors[2] = cells[xCoord, yCoord - 1];
+		}else{
+			cellNeighbors[2] = cells[xCoord, cellsDimensionY - 1];
+		}
+
+		//south cell
+		if(yCoord != cellsDimensionY - 1){
+			cellNeighbors[3] = cells[xCoord, yCoord + 1];
+		}else{
+			cellNeighbors[3] = cells[xCoord, 0];
+		}
+
+		//northwest cell
+		if(xCoord != 0 && yCoord != 0){
+			cellNeighbors[4] = cells[xCoord - 1, yCoord - 1];
+		}else if(xCoord != 0){
+			cellNeighbors[4] = cells[xCoord - 1, cellsDimensionY - 1];
+		}else if(yCoord != 0){
+			cellNeighbors[4] = cells[cellsDimensionX - 1, yCoord - 1];
+		}else{
+			cellNeighbors[4] = cells[cellsDimensionX - 1, cellsDimensionY - 1];
+		}
+
+		//northeast cell
+		if(xCoord != cellsDimensionX - 1 && yCoord != 0){
+			cellNeighbors[5] = cells[xCoord + 1, yCoord - 1];
+		}else if(xCoord != cellsDimensionX - 1){
+			cellNeighbors[5] = cells[xCoord + 1, cellsDimensionY - 1];
+		}else if(yCoord != 0){
+			cellNeighbors[5] = cells[0, yCoord - 1];
+		}else{
+			cellNeighbors[5] = cells[0, cellsDimensionY - 1];
+		}
+
+		//southeast cell
+		if(xCoord != cellsDimensionX - 1 && yCoord != cellsDimensionY - 1){
+			cellNeighbors[6] = cells[xCoord + 1, yCoord + 1];
+		}else if(xCoord != cellsDimensionX - 1){
+			cellNeighbors[6] = cells[xCoord + 1, 0];
+		}else if(yCoord != cellsDimensionY - 1){
+			cellNeighbors[6] = cells[0, yCoord + 1];
+		}else{
+			cellNeighbors[6] = cells[0, 0];
+		}
+
+		//southwest cell
+		if(xCoord != 0 && yCoord != cellsDimensionY - 1){
+			cellNeighbors[7] = cells[xCoord - 1, yCoord + 1];
+		}else if(xCoord != 0){
+			cellNeighbors[7] = cells[xCoord - 1, 0];
+		}else if(yCoord != cellsDimensionY - 1){
+			cellNeighbors[7] = cells[cellsDimensionX - 1, yCoord + 1];
+		}else{
+			cellNeighbors[7] = cells[cellsDimensionX - 1, 0];
+		}
+
+		return cellNeighbors;
+	}
+
+	public float GetCell(int xCoord, int yCoord){
+		return cells[xCoord, yCoord];
+	}
+}
---
+++ Assets/Scripts/ConValue_CA/ConValue_CAView.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ConValue_CAModel))]
+public class ConValue_CAView : MonoBehaviour {
+
+	public float cellBuffer;
+	public float cellSize;
+	public GameObject cellRepresentation;
+
+	private ConValue_CAModel caModel;
+
+	// Use this for initialization
+	void Start () {
+		caModel = gameObject.GetComponent<ConValue_CAModel>();
+
+		for(int i = 0; i < caModel.cellsDimensionX; i++){
+			GameObject cellRow = new GameObject();
+
+			cellRow.transform.parent = gameObject.transform;
+			cellRow.name = "Cell Row " + i;
+
+			for(int j = 0; j < caModel.cellsDimensionY; j++){
+				GameObject cell = Instantiate(cellRepresentation, Vector3.zero, Quaternion.identity) as GameObject;
+
+				cell.transform.parent = cellRow.transform;
+				cell.name = "Cell " + j;
+
+				float xPos = j + (float)(cellSize/2) + (j * cellBuffer);
+				cell.transform.position = new Vector2(xPos, 0);
+			}
+
+			float rowPosY = i + (float)(cellSize + (i * cellBuffer));
+			cellRow.transform.position = new Vector2(0, rowPosY);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < caModel.cellsDimensionX; i++) {
+			for (int j = 0; j < caModel.cellsDimensionY; j++) {
+				float colorValue = caModel.GetCell(i, j);
+				Color newColor = new Color(colorValue, colorValue, colorValue, 1.0f);
+				gameObject.transform.GetChild(i).GetChild(j).gameObject.GetComponent<SpriteRenderer>().color = newColor;
+			}
+		}
+	}
+}
